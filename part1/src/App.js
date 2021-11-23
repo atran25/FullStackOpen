b6975704b6ffae3cb58b61508c20ca9ed0dc18e3
Anthony Tran <hello@anthonytran.me>
2021-11-22 19:27:19 -0800

    Part 1.1 Done

--- part1/src/App.js
+++ part1/src/App.js
@@ -1,22 +1,41 @@
 import React from "react";
 
-const Hello = (props) => {
+
+const Header = (props) => {
   return (
-    <div>
-      <p>Hello {props.name}, you are {props.age} years old</p>
-    </div>
+    <h1>{props.name}</h1>
+  )
+}
+
+const Content = (props) => {
+  return (
+    <p>
+      {props.part_name}: {props.amount}
+    </p>
+  )
+}
+const Total = (props) => {
+  return (
+    <p>Number of exercises: {props.amount_1 + props.amount_2 + props.amount_3}</p>
   )
 }
 
 const App = () => {
-  const name = "Anthony"
-  const age = 10
+  const course = "Half Stack application development"
+  const part1 = "Fundamentals of React"
+  const exercises1 = 10
+  const part2 = "Using props to pass data"
+  const exercises2 = 7
+  const part3 = "State of a component"
+  const exercises3 = 14
 
   return (
     <div>
-      <h1>Greetings</h1>
-      <Hello name={name} age={age} />
-      <Hello name="Anthony" age={18}/>
+      <Header name={course}/>
+      <Content part_name={part1} amount={exercises1} />
+      <Content part_name={part2} amount={exercises2} />
+      <Content part_name={part3} amount={exercises3} />
+      <Total amount_1={exercises1} amount_2={exercises2} amount_3={exercises3} />
     </div>
   )
 }
